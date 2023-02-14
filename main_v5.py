4a10a3089d193ecac64828e1734146cabb43cbca
@IDGS-801-19002027 <19002027@alumnos.utleon.edu.mx>
2023-02-13 19:28:03 -0600

    Sesión del 13 de febrero del 2023 - Corección

--- main_v5.py
+++ main_v5.py
@@ -12,7 +12,7 @@ def resultado():
     n1=request.form.get("txtNum1")
     n2=request.form.get("txtNum2")
     res = int(n1)*int(n2)
-    return render_template("usuarios.html",res,n1,n2)
+    return render_template("resultado.html",res=res,n1=n1,n2=n2)
 
 if __name__ == "__main__":
     app.run(debug = True, port = 3000)
